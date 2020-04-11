5dd5404fe69e283cfe3bc9cfd49e84325ae683af
dzcxs6c <radek.bednarik@tesena.com>
2020-04-11 19:43:54 +0200

    docstrings added

--- parabot/parabot.py
+++ parabot/parabot.py
@@ -13,16 +13,37 @@ from parabot.utils import (
 
 
 def worker(filepath: Any) -> None:
+    """Worker used by Pool.
+
+    Runs instance of RobotFramework for given .robot file.
+    
+    Arguments:
+        filepath {Any} -- python PurePath to .robot file to be executed by robot.run.run
+    
+    See:
+        https://docs.python.org/3/library/pathlib.html
+        https://robot-framework.readthedocs.io/en/v3.1.2/autodoc/robot.html#module-robot.run
+    """
     basepath: Any = get_parent_dir(filepath)
     run(filepath, outputdir=create_output_folder(basepath, filepath.name))
 
 
 def pool_jobs(worker: Callable, filepathslist: List[Any]) -> None:
+    """Runs worker against list of arguments in parallel.
+
+    Max. number of parallel processes is limited by no. of CPUs cores.
+    
+    Arguments:
+        worker {Callable} -- worker function
+        filepathslist {List[Any]} -- list of python PurePaths to .robot files
+    """
     with Pool(maxtasksperchild=1) as p:
         p.map(worker, filepathslist)
 
 
 def main() -> None:
+    """Main function.
+    """
     filepathslist: List[Any]
     args: Any = parse_args()
 
--- parabot/utils.py
+++ parabot/utils.py
@@ -5,6 +5,14 @@ from typing import Any, List
 
 
 def parse_args() -> Any:
+    """Parsers arguments provided by user via CLI.
+    
+    Returns:
+        Any -- object with parsed arguments as attributes
+    
+    See:
+        https://docs.python.org/3/library/argparse.html
+    """
     parser: Any = argparse.ArgumentParser(
         description="What tests do you want to run in parallel?"
     )
@@ -26,20 +34,55 @@ def parse_args() -> Any:
 
 
 def get_parent_dir(test_filepath: Any) -> Any:
+    """Returns PurePath to parent directory of argument filepath.
+    
+    Arguments:
+        test_filepath {Any} -- python Path to .robot file
+    
+    Returns:
+        Any -- python PurePath to parent directory.
+    
+    See:
+        https://docs.python.org/3/library/pathlib.html
+    """
     return PurePath(test_filepath).parent
 
 
 def create_output_folder(basepath: Any, filename: str) -> Any:
+    """Returns PurePath to new folder for report of <filename> provided as argument.
+
+    Actual creation of the directory is handled by RobotFramework.
+    
+    Arguments:
+        basepath {Any} -- PurePath to .robot file parent directory
+        filename {str} -- .robot file filename
+    
+    Returns:
+        Any -- PurePath to report folder of <filename> .robot file
+    """
     timestamp: str = dt.now().strftime("%Y%m%d_%H%M%S")
     dirname: str = "_".join(["report", filename.rstrip(".robot"), timestamp])
     return PurePath(basepath, dirname)
 
 
 def get_all_robot_files() -> List[Any]:
+    """Returns list of Paths of all .robot files in the directory tree.
+    
+    Returns:
+        List[Any] -- Paths of all .robot files in the directory tree.
+    """
     return list(Path(".").rglob("*.robot"))
 
 
 def get_specific_robot_files_by_paths(reldirpaths: List[str]) -> List[Any]:
+    """Returns list of Paths of all .robot files in provided list of relative paths.
+    
+    Arguments:
+        reldirpaths {List[str]} -- list of relative paths to .robot files
+    
+    Returns:
+        List[Any] -- Paths to .robot files
+    """
     output: List[Any] = []
     for path in reldirpaths:
         [output.append(path_) for path_ in list(Path(path).rglob("*.robot"))]
