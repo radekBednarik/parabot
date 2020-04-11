157b4aeecb5d73c5fa3bc2f915cfe2383aba0a44
bednaJedna <bednarik.radek@gmail.com>
2020-04-11 10:01:54 +0200

    find_packages added

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.0.1",
     description="Execute robotframework test files/tests in paralel, even without special preparation of them",
     author="bednaJedna",
+    packages=find_packages(),
     install_requires=["robotframework", "robotframework-seleniumLibrary"],
     license="MIT",
     keywords="robotframework, parallel execution, test, testing",
