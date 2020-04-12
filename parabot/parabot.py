d56be1e20cdbfd624bbceea79325740bb7835762
dzcxs6c <radek.bednarik@tesena.com>
2020-04-12 20:22:09 +0200

    funcs renaming

--- parabot/parabot.py
+++ parabot/parabot.py
@@ -12,7 +12,7 @@ from parabot.utils import (
 )
 
 
-def worker(filepath: Any) -> None:
+def path_worker(filepath: Any) -> None:
     """Worker used by Pool.
 
     Runs instance of RobotFramework for given .robot file.
@@ -28,29 +28,32 @@ def worker(filepath: Any) -> None:
     run(filepath, outputdir=create_output_folder(basepath, filepath.name))
 
 
-def pool_jobs(worker: Callable, filepathslist: List[Any]) -> None:
-    """Runs worker against list of arguments in parallel.
+def pool_path_workers(path_worker: Callable, filepathslist: List[Any]) -> None:
+    """Runs path_worker against list of arguments in parallel.
 
     Max. number of parallel processes is limited by no. of CPUs cores.
     
     Arguments:
-        worker {Callable} -- worker function
+        path_worker {Callable} -- path_worker function
         filepathslist {List[Any]} -- list of python PurePaths to .robot files
     """
     with Pool(maxtasksperchild=1) as p:
-        p.map(worker, filepathslist)
+        p.map(path_worker, filepathslist)
 
 
 def main() -> None:
     """Main function.
+
+    See:
+        https://stackoverflow.com/questions/30487767/check-if-argparse-optional-argument-is-set-or-not
     """
     filepathslist: List[Any]
     args: Any = parse_args()
 
     if args.all:
         filepathslist = get_all_robot_files()
-        pool_jobs(worker, filepathslist)
+        pool_path_workers(path_worker, filepathslist)
 
-    elif hasattr(args, "folders"):
+    if args.folders is not None:
         filepathslist = get_specific_robot_files_by_paths(args.folders)
-        pool_jobs(worker, filepathslist)
+        pool_path_workers(path_worker, filepathslist)
