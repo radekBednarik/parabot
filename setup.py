caa4d44192989bb5b20a7f8a031b6eab2b7232c2
dzcxs6c <radek.bednarik@tesena.com>
2020-04-29 05:57:12 -0700

    added selenium dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Execute robotframework test files/tests in paralel, even without special preparation of them",
     author="bednaJedna",
     packages=find_packages(),
-    install_requires=["robotframework", "robotframework-seleniumLibrary"],
+    install_requires=["robotframework", "selenium", "robotframework-seleniumLibrary"],
     license="MIT",
     keywords="robotframework, parallel execution, test, testing",
 )
