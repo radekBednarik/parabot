9f2e2cf264019692265b3c0d3f994ddcb3a41479
dzcxs6c <radek.bednarik@tesena.com>
2020-05-03 11:38:45 +0200

    bump up version as it goes to pypi

--- parabot/setup.py
+++ parabot/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="parabot",
-    version="1.0.1",
+    version="1.0.2",
     description="Execute robotframework test files/tests in paralel, even without special preparation of them",
     long_description=long_description,
     long_description_content_type="text/markdown",
