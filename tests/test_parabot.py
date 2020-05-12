2858bede38df7527b4652bbc00c6852f326c449d
radekBednarik <radek.bednarik@tesena.com>
2020-05-12 20:05:06 +0200

    wip

--- tests/test_parabot.py
+++ tests/test_parabot.py
@@ -22,11 +22,20 @@ def provide_tag(request) -> str:
     return request.param
 
 
-class TestParabot(object):
-    def test_path_worker(self, provide_robot_filepath):
-        status: Optional[int] = parabot.path_worker(provide_robot_filepath)
-        assert status is None
+@pytest.fixture(scope="module")
+def run_path_worker(provide_robot_filepath) -> Optional[int]:
+    return parabot.path_worker(provide_robot_filepath)
 
-    def test_tag_worker(self, provide_tag):
-        status: Optional[int] = parabot.tag_worker(provide_tag)
-        assert status is None
+
+class TestParabot(object):
+    def test_path_worker(self, run_path_worker):
+        status: Optional[int] = run_path_worker
+        assert status == None
+
+    # def test_path_worker(self, provide_robot_filepath):
+    #     status: Optional[int] = parabot.path_worker(provide_robot_filepath)
+    #     assert status is None
+
+    # def test_tag_worker(self, provide_tag):
+    #     status: Optional[int] = parabot.tag_worker(provide_tag)
+    #     assert status is None
